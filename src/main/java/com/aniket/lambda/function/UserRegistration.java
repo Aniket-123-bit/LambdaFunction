71df2e98438e67d8c1dfb27eb2de266373e857fc
Aniket-123-bit <anisunny782@gmail.com>
2021-07-03 12:47:03 +0530

    Commit LastName valid and not valid Lambda function push

--- src/main/java/com/aniket/lambda/function/UserRegistration.java
+++ src/main/java/com/aniket/lambda/function/UserRegistration.java
@@ -1,11 +1,11 @@
 /**
  * Purpose : User Registration
- *           User will input firstName
+ *           User will input lastName
  *           Validate the user inputs using regex Lambda Function.
  *           If matches, return valid else not valid
  *
  * @author : Aniket Raikwar
- * @since : 02.07.2021
+ * @since : 03.07.2021
  */
 package com.aniket.lambda.function;
 import java.util.Scanner;
@@ -13,31 +13,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface FirstName {
-    void firstNameValidate();
+interface LastName{
+    void lastNameValidate();
 }
 
 
 public class UserRegistration {
     public static void main(String[] args ){
         //Lambda -> operator uses
-        FirstName fName = () -> {
+        LastName lName = () -> {
             Scanner sc = new Scanner(System.in);
 
             String regex = "^[A-Z]{1}[a-z]{3,20}$";
 
-            System.out.println("Enter your name");
+            System.out.println("Enter your last name");
             String name = sc.next();
 
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(name);
             if(matcher.matches()){
-                System.out.println("Given name id is valid");
+                System.out.println("Given last name id is valid");
             }else{
-                System.out.println("Given name id is not valid");
+                System.out.println("Given last name id is not valid");
             }
         };
-        fName.firstNameValidate();
+        lName.lastNameValidate();
 
     }
 }
