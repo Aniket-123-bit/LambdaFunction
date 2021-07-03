cd2843f2cd2cc5d894732faf48e1d47e2fca6fa1
Aniket-123-bit <anisunny782@gmail.com>
2021-07-03 13:14:46 +0530

    Commit Mobile number valid and not valid Lambda function push

--- src/main/java/com/aniket/lambda/function/UserRegistration.java
+++ src/main/java/com/aniket/lambda/function/UserRegistration.java
@@ -1,12 +1,13 @@
 /**
  * Purpose : User Registration
- *           User will input emailValidate
+ *           User will input mobileValidate
  *           Validate the user inputs using regex Lambda Function.
  *           If matches, return valid else not valid
  *
  * @author : Aniket Raikwar
  * @since : 03.07.2021
  */
+
 package com.aniket.lambda.function;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -14,9 +15,8 @@ import java.util.regex.Pattern;
 
 @FunctionalInterface
 interface User{
-    void emailValidate();
+    void mobileValidate();
 }
-
 
 public class UserRegistration {
     public static void main(String[] args ){
@@ -24,20 +24,20 @@ public class UserRegistration {
         User user = () -> {
             Scanner sc = new Scanner(System.in);
 
-            String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+            String regex = "^(91){1}[0-9]{9}$";
 
-            System.out.println("Enter your Email id");
+            System.out.println("Enter your Mobile number");
             String email = sc.next();
 
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(email);
             if(matcher.matches()){
-                System.out.println("Given email id is valid");
+                System.out.println("Given mobile number is valid");
             }else{
-                System.out.println("Given email id is not valid");
+                System.out.println("Given mobile number. is not valid");
             }
         };
-        user.emailValidate();
+        user.mobileValidate();
 
     }
 }
